d7690261dfaeae92c2d728cc85dc9194e7fe5e22
YouJournal <team@youjournal.co.uk>
2024-03-15 21:02:29 +0000

    Initial Commit

---
+++ YouJournal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouJournal
+//
+//  Created by Luke Trotman on 15/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouJournal/YouJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouJournalApp.swift
+//  YouJournal
+//
+//  Created by Luke Trotman on 15/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct YouJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
